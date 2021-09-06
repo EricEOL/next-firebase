ef23ccd680be133346a0fdf7db8a79349e88d3bc
EricEOL <lopes.eric051@gmail.com>
2021-09-06 17:17:29 -0300

    Form component complete

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,6 +12,7 @@ export function Button(props: ButtonProps) {
     <button className={`
       bg-gradient-to-r from-${color}-400 to-${color}-700
       text-white px-4 py-2 rounded-md
+      hover:bg-gradient-to-r hover:from-${color}-700 hover:to-${color}-400
       ${props.className}
     `}>
       {props.children}
